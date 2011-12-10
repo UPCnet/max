2d09ab0b41cd1dddf1589b1771ecf4e572d66d48
Víctor Fernández de Alba <sneridagh@gmail.com>
2011-12-10 12:36:45 +0100

    primera versió del model per mongo

--- max/models.py
+++ max/models.py
@@ -1,3 +1,52 @@
+import time
+from rfc3339 import rfc3339
+
+class Activity(MADBase):
+    
+    collection = 'activity'
+
+    schema = [
+                'actor',
+                'verb',
+                'object'
+            ]
+
+    def __init__(self, *args, **kwargs):
+        self['published'] = rfc3339(time.time())
+        self.update(*args, **kwargs)
+
+class MADBase(dict):
+    """A simple vitaminated dict for holding a MongoBD arbitrary object"""
+    schema = []
+
+    def __init__(self, *args, **kwargs):
+        self.update(*args, **kwargs)
+
+    def __getitem__(self, key):
+        val = dict.__getitem__(self, key)
+        return val
+
+    def __setitem__(self, key, val):
+        if key in schema:
+            dict.__setitem__(self, key, val)
+        else:
+            raise AttributeError
+
+    def __repr__(self):
+        dictrepr = dict.__repr__(self)
+        return '%s(%s)' % (type(self).__name__, dictrepr)
+
+    def update(self, *args, **kwargs):
+        for k, v in dict(*args, **kwargs).iteritems():
+            self[k] = v
+
+    def __setattr__(self, key, value):
+        self.__setitem__(key, value)
+    
+    def __getattr__(self, key):
+        return self.__getitem__(key)
+
+
 # import transaction
 
 # from sqlalchemy.orm import scoped_session
