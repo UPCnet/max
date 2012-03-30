c63908331cbc163f181df459806b21b313108bc7
Víctor Fernández de Alba <victor.fernandez@upcnet.es>
2012-03-30 13:20:24 +0200

    update

--- celeryconfig.py
+++ celeryconfig.py
@@ -2,7 +2,7 @@ BROKER_HOST = "localhost"
 BROKER_PORT = 5672
 BROKER_USER = "maxrabbit"
 BROKER_PASSWORD = "operations"
-BROKER_VHOST = "sneridagh"
+BROKER_VHOST = ""
 
 CELERY_RESULT_BACKEND = "amqp"
 CELERY_IMPORTS = ("maxrules.tasks",)
