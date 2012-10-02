2b12cfc523bf99c78c5b240df31357abe13665c0
Victor Fernandez de Alba <sneridagh@gmail.com>
2012-10-02 17:08:54 +0200

    More detailed debug log for maxrules. (Cherry-picked and adapted from master)
    Conflicts:
    
    	maxrules/tasks.py

--- maxrules/tasks.py
+++ maxrules/tasks.py
@@ -1,5 +1,5 @@
 from celery.task import task
-from maxrules.twitter import twitter_generator_name, debug_hashtag, max_server_url
+from maxrules.twitter import twitter_generator_name, debug_hashtag, max_server_url, logging_file
 import requests
 import pymongo
 from maxrules.config import mongodb_url, mongodb_db_name
@@ -14,6 +14,16 @@ import logging
 def processTweet(twitter_username, content):
     """ Process inbound tweet
     """
+    # Setup logging
+    logger = logging.getLogger("maxrules")
+    fh = logging.FileHandler(logging_file, encoding="utf-8")
+    formatter = logging.Formatter('%(asctime)s %(message)s')
+    logger.setLevel(logging.DEBUG)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+    logger.info("Processing %s tweet with content: %s" % (twitter_username, content))
+
     conn = pymongo.Connection(mongodb_url)
     db = conn[mongodb_db_name]
     users = MADMaxCollection(db.users)
@@ -47,12 +57,15 @@ def processTweet(twitter_username, content):
 
             re = requests.post('%s/admin/contexts/%s/activities' % (max_server_url, url_hash), json.dumps(newactivity), auth=('admin', 'admin'), verify=False)
             if re.status_code == 201:
-                logging.warning("Successful %s tweet from context %s" % (twitter_username, context_url))
-                #return "Successful %s tweet from context %s" % (twitter_username, context_url)
+                # 200: Successful tweet from context
+                logger.info("(201) Successful %s tweet from context %s" % (twitter_username, context_url))
+                return "(201) %s tweet from context %s" % (twitter_username, context_url)
             else:
-                logging.warning("Error accessing the MAX API at %s" % max_server_url)
-                #return "Error accessing the MAX API at %s" % max_server_url
-        return
+                # 500: Error accessing the MAX API
+                logger.info("(500) Error accessing the MAX API at %s" % max_server_url)
+                return "(500) MAX API error"
+
+        return "Should not see me..."
 
     # If we have a tweet from a tracked hashtag
     # Parse text and determine the second or nth hashtag
@@ -63,7 +76,8 @@ def processTweet(twitter_username, content):
     query = [dict(twitterHashtag={'$regex':hashtag, '$options':'i'}) for hashtag in possible_hastags]
 
     if debug_hashtag in possible_hastags:
-        return "Debug hashtag detected! %s" % content
+        logger.info("%s Debug hashtag detected!" % content)
+        return "%s Debug hashtag detected! %s" % content
 
     # Check if twitter_username is a registered for a valid MAX username
     # if not, discard it
@@ -71,7 +85,8 @@ def processTweet(twitter_username, content):
     if maxuser:
         maxuser = maxuser[0]
     else:
-        return "Discarding %s tweet: Not such MAX user" % twitter_username
+        logger.info("(404) Discarding %s tweet: No such MAX user." % twitter_username)
+        return "(404) %s: No such MAX user." % twitter_username
 
     # Check if hashtag is registered for a valid MAX context
     # if not, discard it
@@ -86,10 +101,9 @@ def processTweet(twitter_username, content):
                 can_write = canWriteInContexts(maxuser, [context.object])
             except:
                 can_write = False
+                logger.info("(401) %s can't write to %s" % (maxuser.username, context.object['url']))
 
-            if not can_write:
-                logging.warning("MAX user %s can't write on context %s" % (maxuser, context.object['url']))
-            else:
+            if can_write:
                 # Construct the payload with the activity information
                 newactivity = {
                     "object": {
@@ -107,13 +121,15 @@ def processTweet(twitter_username, content):
                 # MAX context in name of the specified MAX username
                 re = requests.post('%s/admin/people/%s/activities' % (max_server_url, maxuser.username), json.dumps(newactivity), auth=('admin', 'admin'), verify=False)
                 if re.status_code == 201:
-                    logging.warning("Success tweet from user %s in context %s" % (maxuser, context.object['url']))
+                    logger.info("(201) Successful tweet from user %s in context %s" % (maxuser, context.object['url']))
                     successful_tweets += 1
                     #return "Success tweet from user %s in context %s" % (maxuser, context.url)
                 else:
-                    logging.warning("Error accessing the MAX API at %s" % max_server_url)
+                    logger.info("(500) Error accessing the MAX API at %s" % max_server_url)
                     #return "Error accessing the MAX API at %s" % max_server_url
-        error_message = len(maxcontext) == successful_tweets and " We weren't able to send the tweet to all contexts. See log for more information." or ""
-        return "Processed tweets to %d of %d possible contexts.%s" % (successful_tweets, len(maxcontext), error_message)
+        error_message = len(maxcontext) == successful_tweets and " We weren't able to send the tweet to all contexts. See above for more information." or ""
+        logger.info("Processed tweets to %d of %d possible contexts.%s" % (successful_tweets, len(maxcontext), error_message))
+        return "(401) Some tweets not sent"
     else:
-        return "Discarding %s tweet: Not such MAX context" % maxuser.username
+        logger.info("(404) Discarding %s tweet: Not such MAX context %s" % (maxuser.username, maxcontext))
+        return "(404) %s: Not such MAX context %s" % (maxuser.username, maxcontext)
