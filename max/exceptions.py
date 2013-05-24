6eb2e3e8ca21fddef61b8e22a03a71a91b037672
Carles Bruguera <carlesba@gmail.com>
2013-05-24 13:44:25 +0200

    Remove unused code

--- max/exceptions.py
+++ max/exceptions.py
@@ -40,7 +40,7 @@ class JSONHTTPNotImplemented(HTTPNotImplemented):
         self.content_type = 'application/json'
 
 
-class JSONHTTPInternalServerError(HTTPInternalServerError):
+class JSONHTTPInternalServerError(HTTPInternalServerError):  # pragma: no cover
 
     def __init__(self, error):
         Response.__init__(self, json.dumps(error), status=self.code)
@@ -51,77 +51,47 @@ class MissingField(Exception):
     def __init__(self, value):
         self.value = value
 
-    def __str__(self):
-        return repr(self.value)
-
 
 class ObjectNotSupported(Exception):
     def __init__(self, value):
         self.value = value
-
-    def __str__(self):
-        return repr(self.value)
 
 
 class ObjectNotFound(Exception):
     def __init__(self, value):
         self.value = value
 
-    def __str__(self):
-        return repr(self.value)
-
 
 class DuplicatedItemError(Exception):
     def __init__(self, value):
         self.value = value
-
-    def __str__(self):
-        return repr(self.value)
 
 
 class UnknownUserError(Exception):
     def __init__(self, value):
         self.value = value
 
-    def __str__(self):
-        return repr(self.value)
-
 
 class Unauthorized(Exception):
     def __init__(self, value):
         self.value = value
-
-    def __str__(self):
-        return repr(self.value)
 
 
 class InvalidSearchParams(Exception):
     def __init__(self, value):
         self.value = value
 
-    def __str__(self):
-        return repr(self.value)
-
 
 class InvalidPermission(Exception):
     def __init__(self, value):
         self.value = value
-
-    def __str__(self):
-        return repr(self.value)
 
 
 class ValidationError(Exception):
     def __init__(self, value):
         self.value = value
 
-    def __str__(self):
-        return repr(self.value)
-
 
 class Forbidden(Exception):
     def __init__(self, value):
         self.value = value
-
-    def __str__(self):
-        return repr(self.value)
