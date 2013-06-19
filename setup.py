e92fda4c0beba7d3e7fe417a8e40327da0052f27
Victor Fernandez de Alba <sneridagh@gmail.com>
2013-06-19 09:57:53 +0200

    Prepare to release

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ requires = [
 test_requires = ['WebTest', 'mock']
 
 setup(name='max',
-      version='3.4',
+      version='3.4.1',
       description='Activity Stream and Subscription Enhanced Engine (Motor d\'Activitat i subscripcions eXtes)',
       long_description=README + '\n\n' + CHANGES + '\n\n' + CONTRIBUTORS,
       classifiers=[
