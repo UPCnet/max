7fda9dbb9432d43ae6b42ab72ec53d95fc7b10ee
Carles Bruguera <carlesba@gmail.com>
2014-06-11 16:57:32 +0200

    Preparing release 4.0.3

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ requires = [
 test_requires = ['WebTest', 'mock']
 
 setup(name='max',
-      version='4.0.3.dev0',
+      version='4.0.3',
       description='Activity Stream and Subscription Enhanced Engine (Motor d\'Activitat i subscripcions eXtes)',
       long_description=README + '\n\n' + CHANGES + '\n\n' + CONTRIBUTORS,
       classifiers=[
