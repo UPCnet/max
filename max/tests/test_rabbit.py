c822605d3c6ecf05bb5ffc1798fc305b894caa3e
Carles Bruguera <carlesba@gmail.com>
2014-07-17 13:54:07 +0200

    Provide a way to run code from other tests

--- max/tests/test_rabbit.py
+++ max/tests/test_rabbit.py
@@ -9,10 +9,12 @@ from mock import patch
 from max.tests.base import MaxTestBase, MaxTestApp, oauth2Header, mock_post
 from max.tests import test_manager, test_default_security
 from maxcarrot import RabbitClient
+import new
+import sys
 
 skipRabbitTest = partial(
     unittest.skipUnless,
-    os.environ.get('Rabbit', False),
+    os.environ.get('rabbit', False),
     'Skipping due to lack of a Rabbitmq Server'
 )
 
@@ -44,12 +46,28 @@ class FunctionalTests(unittest.TestCase, MaxTestBase):
         pyramid.testing.tearDown()
         self.server.disconnect()
 
+    def run_test(self, test_module_name, test_name):
+        """
+            Runs a test method from another module in a dirty (but awesome) way
+        """
+
+        # Step by step import the module, class and method objects
+        test_module = getattr(sys.modules['max.tests'], test_module_name)
+        test_class = getattr(test_module, 'FunctionalTests')
+        test_method = getattr(test_class, test_name)
+
+        # Create a new function sharin code, name and current globals
+        wrapped_test_method = new.function(test_method.func_code, globals(), test_method.func_name)
+
+        # execute the new method
+        wrapped_test_method(self)
+
     # BEGIN TESTS
 
     @skipRabbitTest()
     def test_create_user(self):
-        username = 'messi'
-        self.testapp.post('/people/%s' % username, "", oauth2Header(test_manager), status=201)
+        self.run_test('test_people', 'test_create_user')
+
         self.server.management.load_exchanges()
         self.assertIn('messi.publish', self.server.management.exchanges_by_name)
         self.assertIn('messi.subscribe', self.server.management.exchanges_by_name)
