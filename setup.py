6654d3e729b8da7f6bdff3c925ebaa4acbc22a2c
Carles Bruguera <carlesba@gmail.com>
2014-07-21 07:52:34 +0200

    Preparing release 4.0.7

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ test_requires = ['WebTest', 'mock', 'robotsuite', 'pyramid_robot']
 docs_requires = ['manuel', 'pygments', 'sphinx_bootstrap_theme']
 
 setup(name='max',
-      version='4.0.7.dev0',
+      version='4.0.7',
       description='Activity Stream and Subscription Enhanced Engine (Motor d\'Activitat i subscripcions eXtes)',
       long_description=README + '\n\n' + CHANGES + '\n\n' + CONTRIBUTORS,
       classifiers=[
